0e50585e2aafbca3a05480d5120073efce8f69b5
Drew Immerman <drewimmerman@gmail.com>
2018-07-08 18:02:32 -0400

    Added kernel, vbo, and loop

--- ogl-base/ogl-base.cpp
+++ ogl-base/ogl-base.cpp
@@ -1,6 +1,27 @@
+#include "oglbase.h"
+#include "Kernel.h"
+#include "VBO.h"
+
+Kernel kernel;
+VBO vbo;
+
+extern float dt;
+
+void loop ()
+{
+
+}
 
 int main()
 {
+	// Create OpenGL Context
+	kernel.create(1024, 768, "ogl-base");
+
+	// Create VBO
+	vbo.create();
+
+	// Run game loop
+	kernel.run();
 
 
 	return 0;
